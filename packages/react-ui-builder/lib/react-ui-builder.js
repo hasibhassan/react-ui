680bdefbd052c7debda0059fe9d8b3352ed4c184
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2021-09-13 00:44:45 -0500

    feat: add css modules support

--- packages/react-ui-builder/lib/react-ui-builder.js
+++ packages/react-ui-builder/lib/react-ui-builder.js
@@ -3,6 +3,7 @@ const rollup = require('rollup')
 const path = require('path')
 const resolve = require('@rollup/plugin-node-resolve').default
 const babel = require('@rollup/plugin-babel').default
+const postcss = require('rollup-plugin-postcss')
 
 const currentWorkingPath = process.cwd()
 const { main, name } = require(path.join(currentWorkingPath, 'package.json'))
@@ -18,6 +19,7 @@ const inputOptions = {
   external: ['react'],
   plugins: [
     resolve(),
+    postcss({ modules: true }),
     babel({
       presets: ['@babel/preset-env', '@babel/preset-react'],
       babelHelpers: 'bundled',
