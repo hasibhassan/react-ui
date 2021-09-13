e50912a9154ad9dcb17e04f9981307557bd533c3
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2021-09-13 00:33:46 -0500

    feat: add the react-ui builder

--- packages/react-ui-builder/lib/react-ui-builder.js
+++ packages/react-ui-builder/lib/react-ui-builder.js
@@ -1,2 +1,48 @@
 #!/usr/bin/env node
-console.log('hello')
+const rollup = require('rollup')
+const path = require('path')
+const resolve = require('@rollup/plugin-node-resolve').default
+const babel = require('@rollup/plugin-babel').default
+
+const currentWorkingPath = process.cwd()
+const { main, name } = require(path.join(currentWorkingPath, 'package.json'))
+
+const inputPath = path.join(currentWorkingPath, main)
+
+// Little workaround to get the package name without the scope
+const fileName = name.replace('@hasibhassan/', '')
+
+// see below for details on the options
+const inputOptions = {
+  input: inputPath,
+  external: ['react'],
+  plugins: [
+    resolve(),
+    babel({
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      babelHelpers: 'bundled',
+    }),
+  ],
+}
+
+const outputOptions = [
+  {
+    file: `dist/${fileName}.cjs.js`,
+    format: 'cjs',
+  },
+  {
+    file: `dist/${fileName}.esm.js`,
+    format: 'esm',
+  },
+]
+
+async function build() {
+  // create the bundle
+  const bundle = await rollup.rollup(inputOptions)
+  // loop through the options and write individual bundles
+  outputOptions.forEach(async (options) => {
+    await bundle.write(options)
+  })
+}
+
+build()
