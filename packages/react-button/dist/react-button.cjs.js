039306a28fa22f568c8c48c9522d4ef252d57ff6
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2021-09-13 21:11:21 -0500

    fix: lerna deps

--- packages/react-button/dist/react-button.cjs.js
+++ packages/react-button/dist/react-button.cjs.js
@@ -8,9 +8,44 @@ function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'defau
 
 var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
 
+function styleInject(css, ref) {
+  if (ref === void 0) ref = {};
+  var insertAt = ref.insertAt;
+
+  if (!css || typeof document === 'undefined') {
+    return;
+  }
+
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  style.type = 'text/css';
+
+  if (insertAt === 'top') {
+    if (head.firstChild) {
+      head.insertBefore(style, head.firstChild);
+    } else {
+      head.appendChild(style);
+    }
+  } else {
+    head.appendChild(style);
+  }
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+}
+
+var css_248z = ".react-button-module_button__2oxCj {\n  color: red;\n}\n";
+var styles = {"button":"react-button-module_button__2oxCj"};
+styleInject(css_248z);
+
 var Button = function Button(_ref) {
   var children = _ref.children;
-  return /*#__PURE__*/React__default['default'].createElement("button", null, children);
+  return /*#__PURE__*/React__default['default'].createElement("button", {
+    className: styles.button
+  }, children);
 };
 
 exports.Button = Button;
--- packages/react-button/lib/react-button.js
+++ packages/react-button/lib/react-button.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import styles from './react-button.module.css'
 
-const Button = ({ children }) => <button>{children}</button>
+const Button = ({ children }) => (
+  <button className={styles.button}>{children}</button>
+)
 
 export { Button }
