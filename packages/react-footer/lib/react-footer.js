120517cca151c363e6e949cc08ef3101d174e2c4
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2021-09-24 21:45:18 -0500

    fix: react-footer styling

--- packages/react-footer/lib/react-footer.js
+++ packages/react-footer/lib/react-footer.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import styles from './react-footer.module.css'
+import { FaGithub } from 'react-icons/fa'
 
-const Footer = ({ children }) => <p>{children}</p>
-
-export { Footer }
+export default function Footer() {
+  return (
+    <footer className={styles.footerContainer}>
+      <div className={styles.stack}>
+        <div className={styles.stackTwo}>
+          <img src="logo.svg" alt="" className={styles.logo} />
+          <FaGithub fontSize="20px" />
+        </div>
+        <p className={styles.copyright}>
+          &copy; {new Date().getFullYear()} Hasib Hassan. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  )
+}
